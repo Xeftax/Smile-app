02bc302cbc65b9ec0d75768016ad22930211e9f2
AdelineL <ankun0935@gmail.com>
2023-06-07 15:59:00 +0200

    datasheet affiche curves pas encore

--- curves.py
+++ curves.py
@@ -1,6 +1,10 @@
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtWidgets import QWidget, QLabel, QComboBox, QApplication
+from PySide6.QtWidgets import QWidget, QLabel, QComboBox, QApplication, QVBoxLayout
+from PySide6.QtCharts import QChart, QChartView, QLineSeries
+from PySide6.QtCore import Qt, QTimer
+import datasheet
+import observer
 
 class CurvesWidget(QtWidgets.QWidget):
     def __init__(self):
@@ -8,9 +12,18 @@ class CurvesWidget(QtWidgets.QWidget):
 
         self.title = QtWidgets.QLabel("Curves")
 
-    # def comboBox(self):
+        layout = QVBoxLayout()
+        self.chart_view = QChartView()
+        layout.addWidget(self.chart_view)
 
-        # self.parameter = QLabel('Lip Lenth',self)
+        self.chart = QChart()
+        self.series = QLineSeries()
+        self.chart.addSeries(self.series)
+
+        self.x = 0
+        self.y = 0
+
+    def comboBox(self):
 
         combo = QComboBox(self)
 
@@ -24,11 +37,18 @@ class CurvesWidget(QtWidgets.QWidget):
         combo.addItem('Smile Index')
 
         combo.move(0,50)
-        # self.parameter.move(50)
 
         self.show
 
+    def updateData(self):
+        coordinate = observer.get("framePosition")
+        self.frameLandmarks = observer.get("faceLandmarks")
+        landmarks = self.frameLandmarks[coordinate].landmark
+        self.x += 1
+        self.y = str((landmarks[61].x-landmarks[78].x)/(landmarks[13].y-landmarks[14].y))
+        self.series.append(self.x, self.y)
+    # def onActivated(self):
 
-    def paintEvent(self, event):
-        painter = QtGui.QPainter(self)
-        painter.fillRect(self.rect(), QtGui.QColor("white"))
+    # def paintEvent(self, event):
+    #     painter = QtGui.QPainter(self)
+    #     painter.fillRect(self.rect(), QtGui.QColor("white"))
